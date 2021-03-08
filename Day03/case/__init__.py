f679be623d0f7c128148762e9048c19fc3343b2e
jason-girl <741425387@qq.com>
2021-03-08 18:26:55 +0800

    feat：新增PO代码

---
+++ Day03/case/__init__.py
@@ -0,0 +1,5 @@
+# _*_ coding:utf-8 _*_
+# File Name: __init__.py
+# Author： Emily
+# Date:  2021/3/8  16:06
+# Description :
---
+++ Day04/base/base.py
@@ -0,0 +1,31 @@
+# _*_ coding:utf-8 _*_
+# File Name: base
+# Author： Emily
+# Date:  2021/3/8  18:14
+# Description :
+
+
+class Base:
+    # 初始化
+    def __init__(self):
+        pass
+
+    # 查找元素方法(提供：点击、输入、获取文本)使用
+    def base_find_element(self, driver):
+        pass
+
+    # 点击方法
+    def base_click(self):
+        pass
+
+    # 输入方法
+    def base_input(self):
+        pass
+
+    # 获取文本方法
+    def base_get_text(self):
+        pass
+
+    # 截图方法
+    def base_get_image(self):
+        pass
